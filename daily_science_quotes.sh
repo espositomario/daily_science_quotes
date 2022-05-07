25bcbf70bb5410ca288d1874de87694edd10ff54
Mario Esposito <104198915+espositomario@users.noreply.github.com>
2022-05-07 15:50:42 +0200

    Add files via upload

---
+++ daily_science_quotes.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+# %j #num day of year
+now=$(date +%d)
+
+if [ $now -eq 01 ] ; then
+printf "\n$(tput setaf 1)The saddest aspect of life right now is that science gathers knowledge faster than society gathers wisdom.\n $(tput setaf 2)--Isaac Asimov\n\n"
+tput init
+
+elif [ $now -eq 02 ] ; then
+printf "\n$(tput setaf 1)The fundamental cause of the trouble is that in the modern world the stupid are cocksure while the intelligent are full of doubt.\n $(tput setaf 2)--Bertrand Russell\n\n"
+tput init
+
+elif [ $now -eq 03 ] ; then
+printf "\n$(tput setaf 1)My friends tell me that I have a tendency to point out problems without offering a solutions, but they never tell me what I should do about it.\n $(tput setaf 2)--Daniel Gilbert\n\n"
+tput init
+
+elif [ $now -eq 04 ] ; then
+printf "\n$(tput setaf 1)What I see of Nature is a magnificent structure that we can comprehend only very imperfectly, and that must fill a thinking person with a feeling of humility\n $(tput setaf 2)--Albert Einstein\n\n"
+tput init
+
+elif [ $now -eq 05 ] ; then
+printf "\n$(tput setaf 1)Rationality has always been a minority, but it is a battle worth fighting.\n $(tput setaf 2)--Piero Angela\n\n"
+tput init
+
+elif [ $now -eq 06 ] ; then
+printf "\n$(tput setaf 1)We are all atheists about most of the gods that humanity has ever believed in. Some of us just go one god further.\n $(tput setaf 2)--Richard Dawkins\n\n"
+tput init
+
+elif [ $now -eq 07 ] ; then
+printf "\n$(tput setaf 1)Be thankful that you have a life, and forsake your vain and presumptuous desire for a second one.\n $(tput setaf 2)--Richard Dawkins\n\n"
+tput init
+
+elif [ $now -eq 08 ] ; then
+printf "\n$(tput setaf 1)Bush and bin Laden are really on the same side: the side of faith and violence against the side of reason and discussion. Both have implacable faith that they are right and the other is evil. Each believes that when he dies he is going to heaven. Each believes that if he could kill the other, his path to paradise in the next world would be even swifter. The delusional "next world" is welcome to both of them. This world would be a much better place without either of them.\n $(tput setaf 2)--Richard Dawkins\n\n"
+tput init
+
+elif [ $now -eq 09 ] ; then
+printf "\n$(tput setaf 1)Many of us saw religion as harmless nonsense. Beliefs might lack all supporting evidence but, we thought, if people needed a crutch for consolation, where s the harm? September 11th changed all that.\n $(tput setaf 2)--Richard Dawkins\n\n"
+tput init
+
+elif [ $now -eq 10 ] ; then
+printf "\n$(tput setaf 1)Any fool can know. The point is to understand.\n $(tput setaf 2)--Albert Einstein\n\n"
+tput init
+
+elif [ $now -eq 11 ] ; then
+printf "\n$(tput setaf 1)Education is not the learning of facts, but the training of the mind to think.\n $(tput setaf 2)--Albert Einstein\n\n"
+tput init
+
+elif [ $now -eq 12 ] ; then
+printf "\n$(tput setaf 1)It is not the strongest of the species that survives, not the most intelligent that survives. It is the one that is the most adaptable to change.\n $(tput setaf 2)--Charles Darwin\n\n"
+tput init
+
+elif [ $now -eq 13 ] ; then
+printf "\n$(tput setaf 1)The best way to have a good idea is to have lots of ideas.\n $(tput setaf 2)--Linus Pauling\n\n"
+tput init
+
+elif [ $now -eq 14 ] ; then
+printf "\n$(tput setaf 1)A Christian telling an atheist they re going to hell is as scary as a child telling an adult they re not getting any presents from Santa.\n $(tput setaf 2)--Ricky Gervais\n\n"
+tput init
+
+elif [ $now -eq 15 ] ; then
+printf "\n$(tput setaf 1)An expert is a person who has made all the mistakes that can be made in a very narrow field.\n $(tput setaf 2)--Niels Bohr\n\n"
+tput init
+
+elif [ $now -eq 16 ] ; then
+printf "\n$(tput setaf 1)We are just an advanced breed of monkeys on a minor planet of a very average star. But we can understand the Universe. That makes us something very special.\n $(tput setaf 2)--Stephen Hawking\n\n"
+tput init
+
+elif [ $now -eq 17 ] ; then
+printf "\n$(tput setaf 1)That which can be asserted without evidence, can be dismissed without evidence.\n $(tput setaf 2)--Christopher Hitchens\n\n"
+tput init
+
+elif [ $now -eq 18 ] ; then
+printf "\n$(tput setaf 1)Everything must be made as simple as possible. But not simpler.\n $(tput setaf 2)--Albert Einstein\n\n"
+tput init
+
+elif [ $now -eq 19 ] ; then
+printf "\n$(tput setaf 1)The good thing about science is that it is true whether or not you believe in it.\n $(tput setaf 2)--Neil deGrasse Tyson\n\n"
+tput init
+
+elif [ $now -eq 20 ] ; then
+printf "\n$(tput setaf 1)In science it often happens that scientists say, You know that is a really good argument; my position is mistaken, and then they would actually change their minds and you never hear that old view from them again. They really do it. It doesn t happen as often as it should, because scientists are human and change is sometimes painful. But it happens every day. I cannot recall the last time something like that happened in politics or religion.\n $(tput setaf 2)--Carl Sagan\n\n"
+tput init
+
+elif [ $now -eq 21 ] ; then
+printf "\n$(tput setaf 1)I have a friend who is an artist and has sometimes taken a view which I don t agree with very well. He ll hold up a flower and say -look how beautiful it is,- and I ll agree. Then he says -I as an artist can see how beautiful this is but you as a scientist take this all apart and it becomes a dull thing,- and I think that he s kind of nutty. First of all, the beauty that he sees is available to other people and to me too, I believe. Although I may not be quite as refined aesthetically as he is ... I can appreciate the beauty of a flower. At the same time, I see much more about the flower than he sees. I could imagine the cells in there, the complicated actions inside, which also have a beauty. I mean it s not just beauty at this dimension, at one centimeter; there s also beauty at smaller dimensions, the inner structure, also the processes. The fact that the colors in the flower evolved in order to attract insects to pollinate it is interesting; it means that insects can see the color. It adds a question: does this aesthetic sense also exist in the lower forms? Why is it aesthetic? All kinds of interesting questions which the science knowledge only adds to the excitement, the mystery and the awe of a flower. It only adds. I don t understand how it subtracts.\n $(tput setaf 2)--Richard P. Feynman\n\n"
+tput init
+
+elif [ $now -eq 22 ] ; then
+printf "\n$(tput setaf 1)A reliable way to make people believe in falsehoods is frequent repetition, because familiarity is not easily distinguished from truth. Authoritarian institutions and marketers have always known this fact.\n $(tput setaf 2)--Christopher Hitchens\n\n"
+tput init
+
+elif [ $now -eq 23 ] ; then
+printf "\n$(tput setaf 1)That which can be asserted without evidence, can be dismissed without evidence.\n $(tput setaf 2)--Daniel Kahneman\n\n"
+tput init
+
+elif [ $now -eq 24 ] ; then
+printf "\n$(tput setaf 1)Never memorize something that you can look up.\n $(tput setaf 2)--Albert Einstein\n\n"
+tput init
+
+elif [ $now -eq 25 ] ; then
+printf "\n$(tput setaf 1)I believe the simplest explanation is, there is no God. No one created the universe and no one directs our fate. This leads me to a profound realization that there probably is no heaven and no afterlife either. We have this one life to appreciate the grand design of the universe and for that, I am extremely grateful.\n $(tput setaf 2)--Stephen Hawking\n\n"
+tput init
+
+elif [ $now -eq 26 ] ; then
+printf "\n$(tput setaf 1)There s as many atoms in a single molecule of your DNA as there are stars in the typical galaxy. We are, each of us, a little universe.\n $(tput setaf 2)--Neil deGrasse Tyson\n\n"
+tput init
+
+elif [ $now -eq 27 ] ; then
+printf "\n$(tput setaf 1)No, our science is no illusion. But an illusion it would be to suppose that what science cannot give us we can get elsewhere.\n $(tput setaf 2)--Sigmund Freud\n\n"
+tput init
+
+elif [ $now -eq 28 ] ; then
+printf "\n$(tput setaf 1)Science flies you to the moon. Religion flies you into buildings.\n $(tput setaf 2)--Victor Stenger\n\n"
+tput init
+
+elif [ $now -eq 29 ] ; then
+printf "\n$(tput setaf 1)In questions of science, the authority of a thousand is not worth the humble reasoning of a single individual.\n $(tput setaf 2)--Galileo Galilei\n\n"
+tput init
+
+elif [ $now -eq 30 ] ; then
+printf "\n$(tput setaf 1)Millions saw the apple fall, Newton was the only one who asked why.\n $(tput setaf 2)--Bernard M. Baruch\n\n"
+tput init
+
+else  
+printf "\n$(tput setaf 1)I noticed even people who claim everything is predestined, and we can do nothing to change it, look before they cross the road.\n $(tput setaf 2)--Stephen Hawking\n\n"
+tput init	
+fi
+
+##make this executable chmod u+x welcome_message.sh
+##simply add the path of this script in your nano ~/.bashrc
+###Created by Mario Esposito 
